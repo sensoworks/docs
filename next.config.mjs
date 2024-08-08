74a06eed9b70708332643d0881f474e6ccd9c898
Alessio Forte <fortealessio@gmail.com>
2024-08-08 13:48:10 +0200

    build: :construction_worker: change next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 import nextra from "nextra";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  assetPrefix: isProd ? "/sensoworks-docs/" : "",
   output: "export",
-}
+  images: {
+    unoptimized: true,
+  },
+};
 
 const withNextra = nextra({
   theme: "nextra-theme-docs",
