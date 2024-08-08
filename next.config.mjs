447bd266eb5c6aa0c9eecf4533e6a55ee7510e1d
Alessio Forte <fortealessio@gmail.com>
2024-08-08 15:24:36 +0200

    feat: :sparkles: add copy code

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,7 @@ const nextConfig = {
 const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
+  defaultShowCopyCode: true,
 });
 
 export default withNextra(nextConfig);
